cc01fcc95c2d324d4780c1e2d031f52ac9ac46c7
Bintang <kadekbintanga@gmail.com>
2021-02-28 00:10:55 +0800

    Develop List Order UI

--- angular-c3/src/app/app.module.ts
+++ angular-c3/src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { OrderlistComponent } from './orderlist/orderlist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderlistComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatIconModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
